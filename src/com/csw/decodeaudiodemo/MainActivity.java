33fbed4ebd52edce00f44fe77aeb3ce253a49aed
lilidejing <469197334@qq.com>
2016-12-16 09:26:33 +0800

    可以解码MP4为h264

---
+++ src/com/csw/decodeaudiodemo/MainActivity.java
@@ -0,0 +1,34 @@
+package com.csw.decodeaudiodemo;
+
+import android.app.Activity;
+import android.media.MediaFormat;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+	private static final String MIME_TYPE = "video/avc"; // H.264 Advanced Video Coding
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		
+		String path=Environment.getExternalStorageDirectory().getAbsolutePath();
+		final AudioCodec audioCodec=AudioCodec.newInstance();
+		audioCodec.setEncodeType(MIME_TYPE);
+		audioCodec.setIOPath(path + "/input.mpg", path + "/encode.h264");
+		audioCodec.prepare();
+		audioCodec.startAsync();
+		audioCodec.setOnCompleteListener(new AudioCodec.OnCompleteListener() {
+		    @Override
+		    public void completed() {
+		        audioCodec.release();
+		    }
+		});
+		
+	}
+
+	
+}
